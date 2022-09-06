ed7bbdcc459dcb91281b72cd693a64a4f2e9b846
yeji seo <yejiseo0725@gmail.com>
2022-09-06 13:56:04 +0900

    update

--- js/main.js
+++ js/main.js
@@ -1,24 +1,33 @@
 $(function () {
+  // Header Slide-Toggle
+  //   $(".gnb ul li .toggle").slideUp();
+  $(".header .inner-header").on("mouseenter", function () {
+    $(".gnb ul li .toggle").stop().slideToggle();
+    // $(".gnb ul li .toggle").slideDown();
+  });
 
-    $('.best .slide-con').slick({
-        dots: true,
-        arrows: true,
-        slidesToShow: 1,
-        // centerPadding: '50%',
-        prevArrow: $('.best .left-btn'),
-        nextArrow: $('.best .right-btn'),
-        variableWidth: true,
-        autoplay: true,
-        autoplaySpeed: 1000,
-        centerMode: true,
-    });
+  $(".header .inner-header").on("mouseleave", function () {
+    $(".gnb ul li .toggle").slideUp();
+  });
 
-    $('.event .event-item').slick({
-        arrows: true,
-        prevArrow: $('.main .event .prev-btn'),
-        nextArrow: $('.main .event .next-btn'),
-        autoplay: true,
-        autoplaySpeed: 1500,
-    });
+  $(".best .slide-con").slick({
+    dots: true,
+    arrows: true,
+    slidesToShow: 1,
+    // centerPadding: '50%',
+    prevArrow: $(".best .left-btn"),
+    nextArrow: $(".best .right-btn"),
+    variableWidth: true,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    centerMode: true,
+  });
 
+  $(".event .event-item").slick({
+    arrows: true,
+    prevArrow: $(".main .event .prev-btn"),
+    nextArrow: $(".main .event .next-btn"),
+    autoplay: true,
+    autoplaySpeed: 1500,
+  });
 });
