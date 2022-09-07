2613dfd08a5f4583e8a008df563ede0965414287
yeji seo <yejiseo0725@gmail.com>
2022-09-07 11:49:03 +0900

    header animation update

--- js/main.js
+++ js/main.js
@@ -1,20 +1,52 @@
 $(function () {
   // Header Slide-Toggle
-  //   $(".gnb ul li .toggle").slideUp();
-  $(".header .inner-header").on("mouseenter", function () {
-    $(".gnb ul li .toggle").stop().slideToggle();
-    // $(".gnb ul li .toggle").slideDown();
+  $(".gnb ul li .toggle").slideUp(500);
+
+  $(".header").on({
+    mouseenter: function () {
+      $(".gnb ul li .toggle").stop().slideDown(500);
+      $(".header .inner-header").addClass("background");
+      $(".header .logo a:last-child img").css({
+        display: "block",
+      });
+      $(".header .gnb ul li a").css({
+        color: "#000",
+      });
+      $(".header .side-menu a i").css({
+        color: "#000",
+      });
+      $(".header .gnb ul li.gnb-list:last-child a.gnb-cat").css({
+        color: "#2b8ab0",
+      });
+    },
+    mouseleave: function () {
+      $(".gnb ul li .toggle").stop().slideUp(500);
+      $(".header .inner-header").removeClass("background");
+      $(".header .logo a:last-child img").css({
+        display: "",
+      });
+      $(".header .gnb ul li a").css({
+        color: "",
+      });
+      $(".header .side-menu a i").css({
+        color: "",
+      });
+    },
   });
 
-  $(".header .inner-header").on("mouseleave", function () {
-    $(".gnb ul li .toggle").slideUp();
+  $(".header .gnb ul li").on({
+    mouseenter: function () {
+      $(this).find("a").addClass("line");
+    },
+    mouseleave: function () {
+      $(this).find("a").removeClass("line");
+    },
   });
 
   $(".best .slide-con").slick({
     dots: true,
     arrows: true,
     slidesToShow: 1,
-    // centerPadding: '50%',
     prevArrow: $(".best .left-btn"),
     nextArrow: $(".best .right-btn"),
     variableWidth: true,
